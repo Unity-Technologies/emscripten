d7c2fa1d01fd023902de6607fae181905b71c65b
Anthony Bowker <anthony.bowker@unity3d.com>
2021-11-30 00:06:19 -0800

    Use .rsp,utf-8 response files

--- emcc.py
+++ emcc.py
@@ -41,7 +41,7 @@ def replace_suffix(f, new_suffix):
 tempfiles = []
 
 def create_response_file(args):
-  response_fd, response_filename = tempfile.mkstemp(suffix='.rsp', text=True)
+  response_fd, response_filename = tempfile.mkstemp(suffix='.rsp.utf-8', text=True)
 
   WINDOWS = (os.name == 'nt')
 
@@ -58,7 +58,7 @@ def create_response_file(args):
     if ' ' in arg:
       arg = '"%s"' % arg
     contents += arg + '\n'
-  with os.fdopen(response_fd, 'w') as f:
+  with os.fdopen(response_fd, 'w', encoding='utf-8') as f:
     f.write(contents)
 
   global tempfiles
